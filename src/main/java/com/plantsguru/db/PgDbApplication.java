04c41f5b1fb25d2cbf9cab62f25f849ac0646f79
Ashish Gupta <ashish.gupta.231990@gmail.com>
2019-07-21 10:27:14 +0530

    project structure

---
+++ src/main/java/com/plantsguru/db/PgDbApplication.java
@@ -0,0 +1,13 @@
+package com.plantsguru.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgDbApplication.class, args);
+	}
+
+}
